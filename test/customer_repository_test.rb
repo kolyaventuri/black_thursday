37ee9b7673ea627fa7d9bec94386b22272a637b7
Kolya Venturi <kolya.venturi@enx3s.com>
2018-02-23 11:17:06 -0700

    Mock CR in tests

--- test/customer_repository_test.rb
+++ test/customer_repository_test.rb
@@ -1,10 +1,11 @@
 require_relative 'test_helper.rb'
 
 require './lib/customer_repository.rb'
+require './test/mocks/test_engine'
 
 class CustomerRepositoryTest < Minitest::Test
   def setup
-    @repo = CustomerRepository.new
+    @repo = CustomerRepository.new MOCK_CUSTOMER_REPOSITORY
     @repo.from_csv './test/fixtures/customers.csv'
   end
 
