9772dcc8caf92ede089209628febbfe6e7f97579
JNelson <nelson.jimmy@gmail.com>
2018-02-23 18:16:35 -0700

    fixed one hound issue

--- test/transaction_test.rb
+++ test/transaction_test.rb
@@ -24,11 +24,14 @@ class TransactionTest < Minitest::Test
     assert_instance_of Transaction, @transaction
   end
 
-  def test_stores_transaction
+  def test_stores_transaction_id_and_credit_card
     assert_equal 6, @transaction.id
     assert_equal 8, @transaction.invoice_id
     assert_equal '4242424242424242', @transaction.credit_card_number
     assert_equal '0220', @transaction.credit_card_expiration_date
+  end
+
+  def test_stores_transaction_time_and_payment_result
     assert_equal 'success', @transaction.result
     assert_equal Time.parse('2016-01-11 17:42:32 UTC'), @transaction.created_at
     assert_equal Time.parse('2016-01-11 17:42:32 UTC'), @transaction.updated_at
