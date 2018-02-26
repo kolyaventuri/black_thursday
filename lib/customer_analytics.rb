e5e34187ef6a14cfce027255152f5a53f78cc293
Kolya Venturi <kolya.venturi@enx3s.com>
2018-02-26 13:38:19 -0700

    Initial customer top merchant method

--- lib/customer_analytics.rb
+++ lib/customer_analytics.rb
@@ -1,3 +1,24 @@
 module CustomerAnalytics
+  def top_merchant_for_customer(id)
+    invoices = @sales_engine.invoices.find_all_by_customer_id id
 
+    invoice_items = invoices.map do |invoice|
+      @sales_engine.invoice_items.find_all_by_invoice_id invoice.id
+    end
+
+    grouped = invoices.group_by(&:merchant_id)
+
+    grouped = grouped.map do |merchant_id, grouped_invoices|
+      quantity = grouped_invoices.map do |invoice|
+        @sales_engine.invoice_items.find_by_id(invoice.id).quantity
+      end.reduce(:+)
+      [merchant_id, quantity]
+    end.to_h
+
+    max = grouped.max_by do |_merch_id, quantity|
+      quantity
+    end
+
+    @sales_engine.merchants.find_by_id max[0]
+  end
 end
