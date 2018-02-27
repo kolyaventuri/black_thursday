52feb50ddc07c639836a1e12dc45304736510bb0
Kolya Venturi <kolya.venturi@enx3s.com>
2018-02-26 23:18:26 -0700

    Updated test

--- test/sales_analyst_test.rb
+++ test/sales_analyst_test.rb
@@ -270,6 +270,6 @@ class SalesAnalystTest < Minitest::Test
     assert_instance_of Array, items
     assert_equal 1, items.length
 
-    assert_equal 1, items.first.id
+    assert_equal 2, items.first.id
   end
 end
